904b890c3eaa79323b3adce450791ae63be777bf
triest <you@example.com>
2019-08-19 23:20:56 +0300

    return json

--- database.php
+++ database.php
@@ -1,14 +1,23 @@
 <?php
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 $mysqli = new mysqli("127.0.0.1", "root", "",
         "phptest");
 if ($mysqli->connect_errno) {
-
     return false;
 }
-$output_array = array();
-$sql = "SELECT * FROM test";
 
+$seach = null;
+if (isset($_GET["seach"])) {
+    $seach = $_GET["seach"];
+}
+
+
+$output_array = array();
+if ($seach == null) {
+    $sql = "SELECT * FROM test";
+} else {
+    $sql = "SELECT * FROM test where name like '%" . $seach . "%'";
+}
 $result = $mysqli->query($sql);
 while ($row = $result->fetch_assoc()) {
     $output_array[] = array('id' => $row['id'], 'name' => $row['name']);
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <h1>Hello World!</h1>
 <label>Seach:</label>
-<input type="text" id="seach" name="seach" onchange="myFunction()" oninput="myFunction()">
+<input type="text" id="seach" name="seach" onchange="myFunction2()" oninput="myFunction2()">
 <table class="table table-bordered" id="dynamic_field">
 
 </table>
@@ -18,8 +18,30 @@
         var table = document.getElementById("dynamic_field");
         table.innerHTML = "";
         $.ajax({
-                url: "database.php",
+                url: "database.php?seach",
                 dataType: "json",
+                data: "data",
+                success: function (data) {
+                    console.log(data);
+                    for (var x = 0; x < data.length; x++) {
+                        $('#dynamic_field').append('<tr id="row">' +
+                            '<tr><td>' + data[x].id + '<td>' + data[x].name + '</td><tr>');
+                    }
+                }
+            }
+        );
+    }
+    ,
+    function myFunction2() {
+        console.log("d");
+        var table = document.getElementById("dynamic_field");
+        var seach = document.getElementById("seach").valueOf().toString();
+
+        table.innerHTML = "";
+        $.ajax({
+                url: "database.php?seach",
+                dataType: "json",
+                data: {seach: seach},
                 success: function (data) {
                     console.log(data);
                     for (var x = 0; x < data.length; x++) {
